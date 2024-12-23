5ccd4589f448be4efa4af783f8520012828be862
sushant <115689963+sushant5101@users.noreply.github.com>
2024-12-23 17:24:55 +0545

    Update hangman1.c
    Made some progress I finally managed to get the easy level function out of my way. I finished the function the function still lacks more things like I have not added the system to display hang man on wrong count. But all the logics seems to be fine till now. I think I will work on the medium level function and now I am thinking it will  be easier to make logic of other level because they might have same functionality as the easy level function.

--- hangman1.c
+++ hangman1.c
@@ -13,7 +13,8 @@ void EASY_LEVEL();
 
 //==================================================adding some global variables=============================
 
-int iLength_of_random_selected_word = 0;
+int iLength_of_random_selected_word = 0,
+    iTemp_count = 0;
 char cUsed_letter[10];
 
 //=================================================creating structure to hold details for repetative task====
@@ -33,50 +34,32 @@ int main()
     int iLevel_index = 0;
 
     printf("Welcome to Hangman!\n");
+    printf("Choose a proper index according to the following list:\n");
+    printf("1. Easy\n2.Medium\n3.Hard\n");
+    scanf("%d", &iLevel_index);
 
-//================================================logic for getting the correct level index==================
+    //============================================calling the apropriate function according to the user input====
 
-    while (1)
+    switch (iLevel_index)
     {
-        printf("Choose a proper index according to the following list:\n");
-        printf("1. Easy\n2.Medium\n3.Hard\n");
-        scanf("%d", &iLevel_index);
+    case 1:
+        EASY_LEVEL();
+        break;
 
-        if (iLevel_index > 3 || iLevel_index < 1)
-        {
-            // system("cls");
-            printf("Invalid index. Please try again.\n");
-            continue;
-        }
-        else
-        {
-            // system("cls");
-            printf("finally some worthy opponent\n");
+    case 2:
+        // MEDIUM_LEVEL();
+        break;
 
-//============================================calling the apropriate function according to the user input====
+    case 3:
+        // HARD_LEVEL();
+        break;
 
-            switch (iLevel_index)
-            {
-            case 1:
-                EASY_LEVEL();
-                break;
-
-            case 2:
-                // MEDIUM_LEVEL();
-                break;
-
-            case 3:
-                // HARD_LEVEL();
-                break;
-
-            default:
-                break;
-            }
-            break;
-        }
+    default:
+        return 1;
+        break;
     }
 
-//=====================================================for now just ending the main function=================
+    //=====================================================for now just ending the main function=================
 
     return 0;
 }
@@ -86,72 +69,64 @@ int main()
 void EASY_LEVEL()
 {
     srand(time(0));
-    int iRand_index = rand() % 10; // (rand() % max - min + 1) + min for random array index each time
+    int iRand_index = rand() % 10; // (rand() % max - min + 1) + min, for random array index each time
+
+    int iRemaining_guesses = 8;
 
     bool bIf_matched = false;
 
-    char cUser_guessed_letter[2]; //--to store the user current entred letter---
+    char cUser_guessed_letter; //--to store the user current entred letter---
 
-    const char *cEasy_word[] = { //---words to choose from in easy level
-        "apple",
-        "chair",
-        "happy",
-        "house",
-        "water",
-        "light",
-        "music",
-        "pizza",
-        "river",
-        "tiger"};
+    const char *cEasy_word[] = {//---words to choose from in easy level
+                                "apple",
+                                "chair",
+                                "happy",
+                                "house",
+                                "water",
+                                "light",
+                                "music",
+                                "pizza",
+                                "river",
+                                "tiger"};
 
-    printf("%s\n", cEasy_word[iRand_index]); //
+    printf("%s\n", cEasy_word[iRand_index]); // just checking
 
     iLength_of_random_selected_word = strlen(cEasy_word[iRand_index]);
 
-    while (1)
-    {
-        printf("Your word is of 5 letter\n");
-        scanf(" %c", &cUser_guessed_letter[0]);
+//================================================Handelling player correct guesses and incorect ================
 
-        for (int i = 0; i < 10; i++)
+    while (iRemaining_guesses > 0)
+    {
+        printf("You have %d reminning guesses\n", iRemaining_guesses);
+        printf("The word has total of %d letters\n", iLength_of_random_selected_word);
+        scanf(" %c", &cUser_guessed_letter);
+
+        for (int i = 0; i < iLength_of_random_selected_word; i++)
         {
-            if (cEasy_word[iRand_index][i] == cUser_guessed_letter[0])
+            if (cUser_guessed_letter == cUsed_letter[i])
             {
-                printf("You have allready guessed the letter : %c\n", cUser_guessed_letter[0]);
-                continue;
+                printf("You have allready guessed the letter %c guess another letter\n", cUser_guessed_letter);
+                bIf_matched = true;
+                break;
             }
-            else
+            else if (cEasy_word[iRand_index][i] == cUser_guessed_letter)
             {
-                cUsed_letter[i] = cUser_guessed_letter[0];
+                cUsed_letter[i] = cUser_guessed_letter;
+                printf("You have found one of the correct letter of the mysterious word\n");
+                break;
             }
-            break;
+            else if (cEasy_word[iRand_index][i] != cUser_guessed_letter)
+            {
+                iTemp_count++;
+            }
+        }
+
+        if (iTemp_count == iLength_of_random_selected_word)
+        {
+            printf("You lost your one guess\n");
+            cUsed_letter[iTemp_count] = cUser_guessed_letter;
+            iRemaining_guesses--;
         }
     }
-
-    for (int i = 0; i < iLength_of_random_selected_word; i++)
-    {
-        if (strchr(cEasy_word[iRand_index], cUser_guessed_letter[0]))
-        {
-            printf("Matched !!\n");
-            bIf_matched = true;
-            for (int j = 0; j < 10; j++)
-            {
-                if (data[j].cCharacter == cEasy_word[iRand_index][i])
-                {
-                    data[j].iCount++;
-                }
-                else
-                {
-                    data[j].cCharacter = cEasy_word[iRand_index][i];
-                    data[j].iCount++;
-                }
-            }
-        }
-        else
-        {
-            printf("did not\n");
-        }
-    }
+//========================================================================================================
 }
-
-//
