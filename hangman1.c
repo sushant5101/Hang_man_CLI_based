f6976a99dd461037a722a36be656aa3e699f3ab9
sushant <115689963+sushant5101@users.noreply.github.com>
2024-12-23 19:11:24 +0545

    Update hangman1.c
    fixed lines of code, that were generating error

--- hangman1.c
+++ hangman1.c
@@ -14,7 +14,8 @@ void EASY_LEVEL();
 //==================================================adding some global variables=============================
 
 int iLength_of_random_selected_word = 0,
-    iTemp_count = 0;
+    iIncorrect_count = 0,
+    iCorrect_count = 0;
 char cUsed_letter[10];
 
 //=================================================creating structure to hold details for repetative task====
@@ -35,7 +36,7 @@ int main()
 
     printf("Welcome to Hangman!\n");
     printf("Choose a proper index according to the following list:\n");
-    printf("1. Easy\n2.Medium\n3.Hard\n");
+    printf("1.Easy\n2.Medium\n3.Hard\n");
     scanf("%d", &iLevel_index);
 
     //============================================calling the apropriate function according to the user input====
@@ -73,7 +74,8 @@ void EASY_LEVEL()
 
     int iRemaining_guesses = 8;
 
-    bool bIf_matched = false;
+    bool bIf_matched = false,
+         bIf_used = false;
 
     char cUser_guessed_letter; //--to store the user current entred letter---
 
@@ -93,40 +95,52 @@ void EASY_LEVEL()
 
     iLength_of_random_selected_word = strlen(cEasy_word[iRand_index]);
 
-//================================================Handelling player correct guesses and incorect ================
+    //================================================Handelling player correct guesses and incorect ================
 
     while (iRemaining_guesses > 0)
     {
-        printf("You have %d reminning guesses\n", iRemaining_guesses);
+        iIncorrect_count = 0;
+        printf("You have %d remaining guesses\n", iRemaining_guesses);
         printf("The word has total of %d letters\n", iLength_of_random_selected_word);
         scanf(" %c", &cUser_guessed_letter);
 
         for (int i = 0; i < iLength_of_random_selected_word; i++)
         {
-            if (cUser_guessed_letter == cUsed_letter[i])
-            {
-                printf("You have allready guessed the letter %c guess another letter\n", cUser_guessed_letter);
-                bIf_matched = true;
-                break;
-            }
-            else if (cEasy_word[iRand_index][i] == cUser_guessed_letter)
+            if (cUsed_letter[i] != cUser_guessed_letter && cUser_guessed_letter == cEasy_word[iRand_index][i])
             {
                 cUsed_letter[i] = cUser_guessed_letter;
-                printf("You have found one of the correct letter of the mysterious word\n");
+                printf("You found a matching letter\n");
+                bIf_matched = true;
+                iCorrect_count++;
+            }
+            else if (cUser_guessed_letter == cUsed_letter[i])
+            {
+                printf("you allready guessed the word: %c\n", cUser_guessed_letter);
+                bIf_used = true;
                 break;
             }
-            else if (cEasy_word[iRand_index][i] != cUser_guessed_letter)
+            else
             {
-                iTemp_count++;
+                iIncorrect_count++;
             }
         }
 
-        if (iTemp_count == iLength_of_random_selected_word)
+        if (iIncorrect_count == iLength_of_random_selected_word)
         {
+            printf("%c was not the letter in the word \n", cUser_guessed_letter);
             printf("You lost your one guess\n");
-            cUsed_letter[iTemp_count] = cUser_guessed_letter;
+            cUsed_letter[iIncorrect_count] = cUser_guessed_letter;
             iRemaining_guesses--;
         }
+        else if (iCorrect_count == iLength_of_random_selected_word)
+        {
+            printf("congratulation you have found word the word was %s\n", cEasy_word[iRand_index]);
+            iRemaining_guesses = 0;
+        }
+        else if (iRemaining_guesses == 0)
+        {
+            printf("You have lost all the remaining guess\n");
+        }
     }
-//========================================================================================================
+    //========================================================================================================
 }
